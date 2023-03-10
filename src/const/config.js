38d6504f858d252110c4667435ed80b484f4ad6b
apple <apple@appledeMacBook-Air.local>
2023-03-10 18:21:12 +0800

    合约地址

--- src/const/config.js
+++ src/const/config.js
@@ -3,7 +3,7 @@ import abi from './abi.json'
 const config = {
     erc20_abi: abi,
     // con_addr: '0x453D7f27f3c5cDBb201bCa91818409023e1ab5Aa'
-    con_addr: '0xF5616D595C1057E94EB03e94f0eE4995954087A1'
+    con_addr: '0xA13C1942Acb968469d9A48504AE217c283f9785A'
 }
 
 export { config }
