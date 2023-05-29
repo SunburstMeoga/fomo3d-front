25e7f473aadfc94e27bc0745d45d4d82ab37dd0a
apple <apple@appledeMacBook-Air.local>
2023-05-29 17:35:55 +0800

    css

--- src/main.js
+++ src/main.js
@@ -10,6 +10,8 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import '@/assets/css/tailwind.css'
 import '@/assets/css/iconfont.css'
+import 'vant/lib/index.css'
+
 import i18n from './local'
 
 
