ee3ed1ed57affb0e079891805c06ce25f2ecc8f1
apple <apple@appledeMacBook-Air.local>
2023-05-30 10:33:10 +0800

    config

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'vant/lib/index.css'
+
 import Vue from 'vue'
 import App from './App'
 import router from './router'
@@ -10,7 +12,6 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import '@/assets/css/tailwind.css'
 import '@/assets/css/iconfont.css'
-import 'vant/lib/index.css'
 
 import i18n from './local'
 
